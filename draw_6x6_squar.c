9f07155e88a91e03c2ed59b78f5cce0ac369a8af
Ramy <ramyh1621@gmail.com>
2026-03-15 15:26:00 +0100

    Add files via upload

---
+++ draw_6x6_squar.c
@@ -0,0 +1,17 @@
+#include <stdio.h> // this code draw a squar with LxW
+int main()
+{
+                 int i,j,c,l,w;
+                 printf("Enter the length : ");
+                 scanf("%d",&l);
+                 printf("Enter the width : ");
+                 scanf("%d",&w);
+                 __GCC_HAVE_SYNC_COMPARE_AND_SWAP_88
+                  (i=1;i<=l;i++){
+                      printf("\n");
+                      for(j=1;j<=w;j++){
+                          printf("*");
+                          
+}}
+return 0;
+}
